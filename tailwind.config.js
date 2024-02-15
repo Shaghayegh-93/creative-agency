1ddd2d3b459b256d83faac73165465b6e1db3f29
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-15 11:05:25 +0000

    add colors as a vatiable

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        backgroundColor: "#0e0c23",
+        textColor: "#ffffff",
+        btn: "#3673fd",
+        bgSoft: "#2d2b42",
+        textSoft: "#e5e5e5",
       },
     },
   },
