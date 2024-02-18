43ef213c76a528121c54b5a74a6710b666048c6f
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-18 22:47:51 +0000

    expad home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import hero from "../../public/hero.gif";
 
-const Home = () => {
-  return <div>Home</div>;
+const HomePage = () => {
+  return (
+    <div className="mt-10">
+      <div className="w-full  flex items-center ">
+        <div className="w-3/4  flex flex-col">
+          <h1 className="text-textSoft text-7xl font-bold w-10 leading-snug ">
+            Creative Thoughts Agancy.
+          </h1>
+          <p className="w-[80%] mt-4">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia hic
+            officiis quidem reprehenderit quis qui, natus voluptatem perferendis
+            expli
+          </p>
+          <div className="flex gap-2 items-center ">
+            <Link href="/" className="">
+              <button className="bg-btn text-white p-2 cursor-pointer rounded-md w-full">
+                Learn More
+              </button>
+            </Link>
+            <Link href="/contact" className="">
+              <button className="bg-textColor text-black p-2 cursor-pointer rounded-md w-full ">
+                Contact
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="w-1/2 h-72 relative">
+          <Image fill src={hero} alt="heroImage" priority />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default HomePage;
