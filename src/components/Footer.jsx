ed7a48b8b25404e9cb126baca1be88d7c7a46b83
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-19 23:24:14 +0000

    add Fotter component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="h-[100px]   flex flex-col md:flex-row items-center md:justify-between text-gray-600">
+      <p className="font-bold">Logo</p>
+      <p className="text-sm"> creative thoughts agancy © All rights reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
