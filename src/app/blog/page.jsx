cf5b95b844089f2e9d2280e91e0e10730baa00c8
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-21 15:51:48 +0000

    map on post data

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import BlogCard from "@/components/BlogCard";
+import {getPostsData} from "../../../server/utils/fetchData";
+// import { connectTodb } from "../../../server/utils/dbConnect";
+// import { Post } from "../../../server/models/models";
 
-const BlogPage = () => {
+const BlogPage = async () => {
+  const posts = await getPostsData();
+  console.log("posts", posts);
   return (
     <div className="grid grid-cols-blog-grid gap-14 ">
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
+      {posts?.map((post) => (
+        <BlogCard key={post?.id} postData={post} />
+      ))}
     </div>
   );
 };
