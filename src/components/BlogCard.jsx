623620915b3273c09c6987ec38db13cc73c9e4da
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-21 16:00:20 +0000

    get data from backend

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -2,21 +2,31 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const BlogCard = () => {
+const BlogCard = ({ postData }) => {
   return (
     <div className="flex flex-col gap-5 mb-10">
       <div className="flex  items-center ">
-        <div className="relative  h-96 w-[90%]">
-          <Image className="object-cover " src="/contact.png" fill />
-        </div>
+        {postData.img && (
+          <div className="relative  h-96 w-[90%]">
+            <Image
+              className="object-cover "
+              alt="postImage"
+              src={postData.img}
+              fill
+            />
+          </div>
+        )}
         <p className="[writing-mode:vertical-rl] text-sm m-auto  w-[10%]">
-          Nov
+          nov
+          {/* {postData.createdAt.toString()} */}
         </p>
       </div>
       <div className="flex flex-1 flex-col gap-2">
-        <h2>title</h2>
-        <p className="text-sm truncate text-gray-300 font-thin">desc</p>
-        <Link className="underline" href="/blog/post">
+        <h2>{postData.title}</h2>
+        <p className="text-sm truncate text-gray-300 font-thin">
+          {postData.desc}
+        </p>
+        <Link className="underline" href={`/blog/${postData.slug}`}>
           READ MORE
         </Link>
       </div>
